8b43fcc9251312ebae60204d762387582f3dbabb
hsinyinfu <peter0930260535@gmail.com>
2019-06-12 05:23:25 +0000

    Remove som redundant feature codes

--- src/MySegmentTree.java
+++ src/MySegmentTree.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 
 public class MySegmentTree {
-    public static void showArray( int[] data )
-    {
-        for( int i=0; i<data.length; ++i )
-            System.out.print( data[i] + " " );
-    }
+    //public static void showArray( int[] data )
+    //{
+    //    for( int i=0; i<data.length; ++i )
+    //        System.out.print( data[i] + " " );
+    //}
     public static int[] buildTree( int idx, int left, int right, int[] data, int[] tree )
     {
         if( left == right )
@@ -52,28 +52,28 @@ public class MySegmentTree {
         ans += rangeSum( mid+1, end, tree, rightIdx, mid+1, right );
         return ans;
     }
-    public static void main(String[] args) {
-        int[] data = { 1, 2, 3 };
-        int[] tree = new int[ data.length*4 ];
-        tree = buildTree( 0, 0, data.length-1, data, tree );
-        System.out.print("Data: ");
-        showArray( data );
-        System.out.print("\nTree: ");
-        showArray( tree );
-        System.out.print("\n");
-		try{
-        	System.out.println( rangeSum( 0, 0, tree, 0, 0, data.length-1 ) );
-        	System.out.println( rangeSum( 0, 1, tree, 0, 0, data.length-1 ) );
-        	System.out.println( rangeSum( 0, 2, tree, 0, 0, data.length-1 ) );
-        	System.out.println( rangeSum( 0, 3, tree, 0, 0, data.length-1 ) );
-        	System.out.println( rangeSum( 1, 1, tree, 0, 0, data.length-1 ) );
-        	System.out.println( rangeSum( 1, 2, tree, 0, 0, data.length-1 ) );
-        	System.out.println( rangeSum( 1, 3, tree, 0, 0, data.length-1 ) );
-        	System.out.println( rangeSum( 2, 2, tree, 0, 0, data.length-1 ) );
-        	System.out.println( rangeSum( 2, 3, tree, 0, 0, data.length-1 ) );
-        	System.out.println( rangeSum( 3, 3, tree, 0, 0, data.length-1 ) );
-		} catch (IllegalArgumentException e1){
-			System.out.println( "Illegal Arguments for rangeSum()\n" );
-		}
-    }
+    //public static void main(String[] args) {
+    //    int[] data = { 1, 2, 3 };
+    //    int[] tree = new int[ data.length*4 ];
+    //    tree = buildTree( 0, 0, data.length-1, data, tree );
+    //    System.out.print("Data: ");
+    //    showArray( data );
+    //    System.out.print("\nTree: ");
+    //    showArray( tree );
+    //    System.out.print("\n");
+	//	try{
+    //    	System.out.println( rangeSum( 0, 0, tree, 0, 0, data.length-1 ) );
+    //    	System.out.println( rangeSum( 0, 1, tree, 0, 0, data.length-1 ) );
+    //    	System.out.println( rangeSum( 0, 2, tree, 0, 0, data.length-1 ) );
+    //    	System.out.println( rangeSum( 0, 3, tree, 0, 0, data.length-1 ) );
+    //    	System.out.println( rangeSum( 1, 1, tree, 0, 0, data.length-1 ) );
+    //    	System.out.println( rangeSum( 1, 2, tree, 0, 0, data.length-1 ) );
+    //    	System.out.println( rangeSum( 1, 3, tree, 0, 0, data.length-1 ) );
+    //    	System.out.println( rangeSum( 2, 2, tree, 0, 0, data.length-1 ) );
+    //    	System.out.println( rangeSum( 2, 3, tree, 0, 0, data.length-1 ) );
+    //    	System.out.println( rangeSum( 3, 3, tree, 0, 0, data.length-1 ) );
+	//	} catch (IllegalArgumentException e1){
+	//		System.out.println( "Illegal Arguments for rangeSum()\n" );
+	//	}
+    //}
 }
